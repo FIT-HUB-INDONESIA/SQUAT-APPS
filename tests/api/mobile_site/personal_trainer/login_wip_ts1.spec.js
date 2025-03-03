2feff0eeb2b7cda1c8d954fce072aa568b3032b0
dayadiqa <dayadiqa@gmail.com>
2025-03-04 01:17:04 +0700

    OPX-1337 feat(pt): api mobile-site create pt-session activity

--- tests/api/mobile_site/personal_trainer/login_wip_ts1.spec.js
+++ tests/api/mobile_site/personal_trainer/login_wip_ts1.spec.js
@@ -1,5 +1,5 @@
-import AuthApiCollections from "../../../../utils/api/mobile_site/authorization/auth";
-import PersonalTrainerApi from "../../../../utils/api/mobile_site/personal_trainer/personal_trainer";
+import AuthApiCollections from "../../../../utils/api/fhad/authorization/auth";
+import PersonalTrainerApi from "../../../../utils/api/fhad/mobile_site/personal_trainer/personal_trainer";
 import allureReporter from "@wdio/allure-reporter";
 import { dotenvConf } from "../../../../config/dotenv";
 
@@ -8,7 +8,7 @@ import { dotenvConf } from "../../../../config/dotenv";
  * Write test-case title in "it" block.
  */
 describe("Test", () => {
-    it("Test case @api", async () => {
+    it("Test case", async () => {
         allureReporter.addParentSuite("Regression");
         allureReporter.addParentSuite("Smoke");
         allureReporter.addSeverity("critical");
@@ -36,6 +36,52 @@ describe("Test", () => {
             type_id,
             started_at,
             finished_at
+        );
+    });
+
+    it("Test case title @api", async () => {
+        allureReporter.addParentSuite("Regression");
+        allureReporter.addParentSuite("Smoke");
+        allureReporter.addSeverity("critical");
+
+        const email = dotenvConf.fhadAdminEmail;
+        const password = dotenvConf.fhadAdminPassword;
+        const club_name = "FIT HUB Bendungan Hilir (Benhil)";
+        const pt_id = "firapt@test.id";
+        const member_phone = "+62817345958";
+        const schedule_at = "2025-03-03 10:45:20 GMT+07:00";
+
+        const auth_login = await AuthApiCollections.post_v1_auth_login(
+            email,
+            password
+        );
+        const token = auth_login.data.data.token;
+
+        const club_id = await PersonalTrainerApi.map_get_v2_clubs(
+            token,
+            club_name
+        );
+
+        const user_id = await PersonalTrainerApi.map_get_v2_clients(
+            token,
+            pt_id,
+            member_phone
+        );
+
+        const active_packages = await PersonalTrainerApi.get_v2_active_packages(
+            token,
+            pt_id,
+            user_id
+        );
+        const user_pt_membership_id = active_packages.data?.data[0].id;
+
+        await PersonalTrainerApi.post_v2_client_schedules(
+            token,
+            club_id,
+            pt_id,
+            schedule_at,
+            user_id,
+            user_pt_membership_id
         );
     });
 });
