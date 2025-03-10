dfef6fc33d9decd24dde6b7a3df98e8a0c043af2
dayadiqa <dayadiqa@gmail.com>
2025-03-11 01:06:17 +0700

    OPX-1351 feat(pt): ui mobile-site book pt-session

--- pages/fhad/page_pt/pt.js
+++ pages/fhad/page_pt/pt.js
@@ -7,21 +7,21 @@ class PtSelectors {
     get pt_club_select_dropdown() {
         return $('//*[@data-testid="ptschedule_filter_club-dropdown"]');
     }
-    get pt_pt_name_select_dropdown() {
-        return $('//*[@data-testid="ptschedule_filter_pt-dropdown"]');
-    }
-    //NOTE - Ini dipakai untuk pengecekan getText
+    //NOTE - getText testing purposes
     get pt_club_select_list() {
         return $('//*[@data-testid="ptschedule_club-dropdown-bottom-sheet"]');
-    }
-    //NOTE - Ini dipakai untuk pengecekan getText
-    get pt_pt_name_select_list() {
-        return $('//*[@data-testid="ptschedule_pt-dropdown-bottom-sheet"]');
     }
     pt_club_select_list_target(preferred_club) {
         return $(
             `//*[@data-testid="ptschedule_club-dropdown-bottom-sheet"]//*[contains(text(), '${preferred_club}')]`
         );
+    }
+    get pt_pt_name_select_dropdown() {
+        return $('//*[@data-testid="ptschedule_filter_pt-dropdown"]');
+    }
+    //NOTE - getText testing purposes
+    get pt_pt_name_select_list() {
+        return $('//*[@data-testid="ptschedule_pt-dropdown-bottom-sheet"]');
     }
     pt_pt_name_select_list_target(preferred_pt_name) {
         return $(
@@ -30,6 +30,36 @@ class PtSelectors {
     }
     get pt_buat_jadwal_button() {
         return $('//*[@data-testid="ptschedule_add-new-schedule-button"]');
+    }
+    get pt_activity_type_dropdown() {
+        return $('(//*[@aria-label="button select"])[3]');
+    }
+    //NOTE - getText testing purposes
+    get pt_activity_type_list() {
+        return $('(//*[@class="bottom-sheet__content"])[2]');
+    }
+    pt_activity_type_list_target(preferred_activity_type) {
+        return $(
+            `(//*[@class="bottom-sheet__content"])[2]//*[contains(text(), '${preferred_activity_type}')]`
+        );
+    }
+    get pt_date_picker_button() {
+        return $('(//*[@aria-label="button datepicker"])[2]');
+    }
+    //NOTE - getText testing purposes
+    get pt_date_picker_list() {
+        return $('//*[@aria-label="calendar picker"]/div[2]');
+    }
+    pt_date_picker_list_target(preferred_date) {
+        return $(
+            `//*[@aria-label="calendar picker"]//*[@style="color: rgb(16, 24, 32); margin: 0px; text-align: center;" and text()='${preferred_date}']`
+        );
+    }
+    get pt_started_at_button() {
+        return $('(//*[@aria-label="button timepicker"])[1]');
+    }
+    get pt_finished_at_button() {
+        return $('(//*[@aria-label="button timepicker"])[2]');
     }
 }
 
@@ -75,6 +105,41 @@ class PtAction extends PtValidation {
             this.pt_buat_jadwal_button,
             `pt_buat_jadwal_button`,
             `Successfully show buat jadwal form bottomsheet`
+        );
+    }
+    async click_pt_activity_type_dropdown() {
+        await elementHelper.click(
+            this.pt_activity_type_dropdown,
+            `pt_activity_type_dropdown`,
+            `Successfully show tipe aktivitas list bottomsheet`
+        );
+    }
+    async click_pt_activity_type_list_target(preferred_activity_type) {
+        await elementHelper.click(
+            this.pt_activity_type_list_target(preferred_activity_type),
+            `pt_activity_type_list_target`,
+            `Successfully select preferred activity type: ${preferred_activity_type}`
+        );
+    }
+    async click_pt_date_picker_button() {
+        await elementHelper.click(
+            this.pt_date_picker_button,
+            `pt_date_picker_button`,
+            `Successfully show date picker bottomsheet`
+        );
+    }
+    async click_pt_date_picker_list_target(preferred_date) {
+        await elementHelper.click(
+            this.pt_date_picker_list_target(preferred_date),
+            `pt_date_picker_list_target`,
+            `Successfully select preferred date: ${preferred_date}`
+        );
+    }
+    async click_pt_started_at_button() {
+        await elementHelper.click(
+            this.pt_started_at_button,
+            `pt_started_at_button`,
+            `Successfully show time picker bottomsheet`
         );
     }
 }
--- tests/fhad/personal_trainer/personal_trainer_activities_ts1.spec.js
+++ tests/fhad/personal_trainer/personal_trainer_activities_ts1.spec.js
@@ -1,6 +1,6 @@
 // import Cleanup from "../../../handlers/sessions_cleanup";
 import allureReporter from "@wdio/allure-reporter";
-// import { dotenvConf } from "../../../config/dotenv";
+import { dotenvConf } from "../../../config/dotenv";
 import login from "../../../pages/fhad/page_login/login";
 import navigation from "../../../pages/fhad/navigation";
 import pt from "../../../pages/fhad/page_pt/pt";
@@ -16,15 +16,17 @@ describe("Feature Name", () => {
         allureReporter.addParentSuite("Smoke");
         allureReporter.addSeverity("critical");
 
-        // const user_email = dotenvConf.fhadAdminEmail;
-        // const user_password = dotenvConf.fhadAdminPassword;
+        const user_email = dotenvConf.fhadAdminEmail;
+        const user_password = dotenvConf.fhadAdminPassword;
         const preferred_club = "FIT HUB Bendungan Hilir (Benhil)";
         const preferred_pt_name = "Fira";
+        const preferred_activity_type = "PT Session";
+        const preferred_date = "27";
 
         await navigation.navigate_to_fithub_dashboard();
-        // await login.addValue_login_email_field(user_email);
-        // await login.addValue_login_password_field(user_password);
-        // await login.click_login_sign_in_button();
+        await login.addValue_login_email_field(user_email);
+        await login.addValue_login_password_field(user_password);
+        await login.click_login_sign_in_button();
 
         await login.skip_google_password_manager_popup();
 
@@ -33,5 +35,10 @@ describe("Feature Name", () => {
         await pt.click_pt_pt_name_select_dropdown();
         await pt.click_pt_pt_name_select_list_target(preferred_pt_name);
         await pt.click_pt_buat_jadwal_button();
+        await pt.click_pt_activity_type_dropdown();
+        await pt.click_pt_activity_type_list_target(preferred_activity_type);
+        await pt.click_pt_date_picker_button();
+        await pt.click_pt_date_picker_list_target(preferred_date);
+        await pt.click_pt_started_at_button();
     });
 });
